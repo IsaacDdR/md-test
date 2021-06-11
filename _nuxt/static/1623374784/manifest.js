39a2d9c9d5c5a98fc85448784f24be8e7af944c8
isaacddr <isaacovic00@gmail.com>
2021-06-10 21:15:35 -0500

    Estilizado

--- _nuxt/static/1623374784/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fnosotros","\u002Fblog","\u002Fgaleria","\u002Fpaquetes","\u002Fcontacto","\u002Fentradas\u002Fgoodbye","\u002Fentradas\u002Fnewtrait","\u002Fentradas\u002Fpost","\u002Fentradas\u002Fhello"]})
